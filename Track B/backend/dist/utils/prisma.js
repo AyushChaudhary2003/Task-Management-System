0710630c7b6fc6276de3e1d1179bdda775966d1a
Ayush Chaudhary <ayushiaf.ac@gmail.com>
2026-04-04 02:10:53 +0530

    chore(Track B): untrack prisma generated files

--- Track B/backend/dist/utils/prisma.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// @ts-ignore
-const client_1 = require("../generated/client");
-// @ts-ignore
-const prisma = new client_1.PrismaClient();
-exports.default = prisma;
